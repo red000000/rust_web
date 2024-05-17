a23940bd0980fbab3020aa084d2da4bfdb15b0c7
Mixtlight <lefannces@gmail.com>
2024-05-17 10:06:58 +0800

    注释更新

--- face/评论模块（无前后端交互）/Js/public.js
+++ face/评论模块（无前后端交互）/Js/public.js
@@ -1,21 +1,22 @@
-
+//基本变量
 const area = document.getElementById("area");
 const add = document.getElementById("add");
 const text = document.getElementById("text");
-const arr = [];
+const arr = []; //存储消息对象
 
 //添加
 add.addEventListener('click', (e) => {
     e.preventDefault();
     if (text.value) {
+        // 读取、设置消息对象
         const obj = {
             personid: arr.length + 1,
             username: "和光予序",
             content: text.value
         }
-        arr.push(obj);
-        render();
-        text.value = '';
+        arr.push(obj); // 向数组末尾添加对象
+        render(); // 再次渲染
+        text.value = ''; // 清空输入框
     }
 })
 //回车输入，但是有bug
@@ -38,19 +39,21 @@ add.addEventListener('click', (e) => {
 //删除
 area.addEventListener('click', (e) => {
     if (e.target.value) {
+        // 将需要删除的对象替换为下一个对象
         for (let i = e.target.id - 1; i < arr.length; i++) {
             arr[i] = arr[i + 1];
         }
-        arr.pop();
-        render();
+        arr.pop(); // 删除数组末尾对象
+        render(); // 再次渲染
     }
 })
 
 //渲染
 function render() {
-    area.innerHTML = '';
+    area.innerHTML = ''; // 渲染前清空
     for (let i = 0; i < arr.length; i++) {
         const tr = document.createElement("tr");
+        // 渲染对象的格式
         tr.innerHTML = `
             <td class="td">
                 <img src="../Img/莉莉丝.jpg" alt="userimg" class="userimg">
@@ -64,6 +67,7 @@ function render() {
                 </div>
             </td>
         `
+        // 渲染对象
         area.appendChild(tr);
     }
 }
